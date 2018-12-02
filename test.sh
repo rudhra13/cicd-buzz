b0a31558c849acc54769e8879587167e40d48df3
Rudhra Rai <rudhrarai@IT000585.local>
2018-12-03 00:47:28 +0530

    test file added

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pwd
 echo "hello"
-mkdir /home/vagrant/testing
-mv test.sh /home/vagrant/testing
-bash /home/vagrant/testing/test.sh
+mkdir ~/testing
+mv test.sh ~/testing
+bash ~/testing/test.sh
 
